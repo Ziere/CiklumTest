221f4353833e89a93268cbd57b484b61efad133a
Ziere <fco.moreno.arcas@gmail.com>
2017-04-27 20:47:02 +0200

    changing 4 spaces to 2

--- processCommandLine.js
+++ processCommandLine.js
@@ -10,25 +10,25 @@ const regex = /^user=(.*)/;
  * @return {String} username
  */
 const processCommandLine = ((inputArguments) => {
-    let result;
-    let username;
-    if (typeof inputArguments === 'string') {        
-        result = regex.test(inputArguments);
+  let result;
+  let username;
+  if (typeof inputArguments === 'string') {
+    result = regex.test(inputArguments);
+    if (result) {
+      username = inputArguments.match(regex)[1];
+    }
+  } else if (typeof inputArguments === 'object') {
+    // check if the object is an Array
+    if (Object.prototype.toString.call(inputArguments) === '[object Array]') {
+      inputArguments.forEach((element) => {  
+        result = regex.test(element);
         if (result) {
-            username = inputArguments.match(regex)[1];
+          username = element.match(regex)[1];
         }
-    } else if (typeof inputArguments === 'object') {
-        // check if the object is an Array
-        if (Object.prototype.toString.call(inputArguments) === '[object Array]') {
-            inputArguments.forEach((element) => {  
-                result = regex.test(element);
-                if (result) {
-                    username = element.match(regex)[1];
-                }
-            });
-        }
-    }    
-    return username;
+      });
+    }
+  }  
+  return username;
 })
 
 module.exports = processCommandLine;
